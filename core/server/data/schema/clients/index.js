952e3b93930becb2cd3b010b0acbb68623447c54
Admin <admin@Admins-Mac-mini.local>
2021-07-21 23:01:01 -0700

    Export postgres module from index

--- core/server/data/schema/clients/index.js
+++ core/server/data/schema/clients/index.js
@@ -1,7 +1,9 @@
 const sqlite3 = require('./sqlite3');
 const mysql = require('./mysql');
+const pg = require('./postgres');
 
 module.exports = {
     sqlite3: sqlite3,
-    mysql: mysql
+    mysql: mysql,
+    pg: pg,
 };
